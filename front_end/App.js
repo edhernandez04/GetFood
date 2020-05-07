150e7023cbb4026543e8b705f013e4b625e4f483
edhernandez04 <mchh.004@gmail.com>
2020-05-07 15:08:53 -0400

    start of the home page

--- front_end/App.js
+++ front_end/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { AppRegistry, View, AsyncStorage } from 'react-native';
-import { Actions, Router, Scene } from 'react-native-router-flux';
+import { Actions, Router, Scene, Stack } from 'react-native-router-flux';
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 
-const App = () => (
+export default class App extends React.Component {
+
+  render(){
+    return(
   <Router>
     <Stack key="root">
       <Scene key="login" component={Login} title="Login" />
@@ -13,6 +16,8 @@ const App = () => (
       <Scene key="home" component={Home} initial={true}/>
     </Stack>
   </Router>
-);
+    )
+  }
+};
 
 AppRegistry.registerComponent('App', () => App)
--- front_end/pages/Home.js
+++ front_end/pages/Home.js
@@ -1,34 +1,61 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, ScrollView } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
 export default class Home extends React.Component {
 
     render(){
         return(
+        <View>
             <View style={styles.restCategory}>
-                <Button>Fast Food</Button>
-                <Button>American</Button>
-                <Button>Chicken</Button>
-                <Button>Breakfast</Button>
-                <Button>Sandwiches</Button>
-                <Button>Healthy</Button>
-                <Button>Mexican</Button>
-                <Button>Pizza</Button>
-                <Button>Chinese</Button>
-                <Button>Seafood</Button>
-                <Button>Kosher</Button>
-                <Button>Jamaican</Button>
-                <Button>Indian</Button>
-                <Button>Asian</Button>
+                <ScrollView horizontal={true}>
+                    <Button title={'Fast Food'}/>
+                    <Button title={'American'}/>
+                    <Button title={'Chicken'}/>
+                    <Button title={'Breakfast'}/>
+                    <Button title={'Sandwiches'}/>
+                    <Button title={'Healthy'}/>
+                    <Button title={'Mexican'}/>
+                    <Button title={'Pizza'}/>
+                    <Button title={'Chinese'}/>
+                    <Button title={'Seafood'}/>
+                    <Button title={'Kosher'}/>
+                    <Button title={'Jamaican'}/>
+                    <Button title={'Indian'}/>
+                    <Button title={'Asian'}/>
+                </ScrollView>
             </View>
+            <View style={styles.recentOrders}>
+                <Text>Recent Places You Ordered From</Text>
+                <Image style={styles.recentRest} source={{uri: 'https://www.ahstatic.com/photos/1146_rsr003_00_p_1024x768.jpg'}}/>
+            </View>
+            <View style={styles.suggestedPlaces}>
+                <Text>Check these places out</Text>
+                <Image style={styles.suggestedRest} source={{uri: 'https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'}}/>
+            </View>
+        </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
     restCategory: {
-        flex: 1,
         flexDirection: 'row'
+    },
+    recentOrders: {
+        alignItems: "center",
+        padding: 25
+    },
+    recentRest: {
+        height: 200,
+        width: 200
+    },
+    suggestedPlaces: {
+        alignItems: "center",
+        padding: 25
+    },
+    suggestedRest: {
+        height: 200,
+        width: 200        
     }
 })
